913848e15a8eea3a5a1ff9d491d935f8dde720db
Andrei Manolache <varasatt@gmail.com>
2018-05-16 00:58:51 +0200

    removed for pr

--- src/config/config.php
+++
@@ -1,5 +0,0 @@
-<?php
-$uploadLocation = "../../uploads";
-openlog('myapplication', LOG_NDELAY, LOG_USER);
-
-?>
--- src/config/database.php
+++
@@ -1,6 +0,0 @@
-<?php
-$mysqli = new mysqli("localhost", "root", "", "infinity");
-if ($mysqli->connect_errno) {
-    echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
-}
-
